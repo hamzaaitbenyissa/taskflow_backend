570138a32e89e67747dcb8c49f19ce5b337f21d5
benyissa <benyissa@ostorlab.dev>
2025-04-15 00:15:55 +0100

    feat: register the tasks app into django admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+"""This module registers the Task model with the Django admin interface."""
+
+from django.contrib import admin
+from tasks import models as tasks_models
+
+
+@admin.register(tasks_models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "completed", "created_at")
+    list_filter = ("completed",)
+    search_fields = ("title", "description")
