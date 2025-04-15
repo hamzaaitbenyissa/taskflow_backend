50c90d4c9ad6853dfff329342baa81d307758eba
benyissa <benyissa@ostorlab.dev>
2025-04-15 01:21:07 +0100

    ruff format .

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rest_framework  import routers as rest_routers
+from rest_framework import routers as rest_routers
 
 from tasks.views import TaskViewSet
 
